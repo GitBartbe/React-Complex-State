2d709fbf0d5b607f0c0dc5a53595c7b0816de526
Bartosz Bednarski <78264646+GitBartbe@users.noreply.github.com>
2021-11-08 21:38:28 +0100

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleHange(e) {
+    const { value, name } = e.target;
+
+    //This line of code remember previous value
+    setFullName((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        };
+      }
+    });
+  }
+  return (
+    <div className="container">
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
+      <form>
+        <input
+          onChange={handleHange}
+          name="fName"
+          placeholder="First Name"
+          // value={fullName.fName}
+        />
+
+        <input
+          onChange={handleHange}
+          name="lName"
+          placeholder="Last Name"
+          // vlaue={fullName.lName}
+        />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
